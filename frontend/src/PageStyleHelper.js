62f222d78597ec7acf9bc3a3a2be8f1344f83849
Tomas Daniel <52777834+Dandys3900@users.noreply.github.com>
2023-10-20 18:37:02 +0200

    Actual code changes. (#3)
    * Actual code changes.
    
    * Update for making slider and switch values persistent using Cookies.
    
    * Add vacation planner idea to docs
    
    * Fixed grammar
    
    * Add survey questions
    
    * Update for making slider and switch values persistent using Cookies.
    
    * Vulnerability fixes.
    
    ---------
    
    Co-authored-by: Dandys <tomasdaniel7800@gmail.com>
    Co-authored-by: Martan03 <martinslezak03@gmail.com>

---
+++ frontend/src/PageStyleHelper.js
@@ -0,0 +1,68 @@
+import Cookies from "js-cookie";
+
+export const Helper = {
+    updateFontSize(diffValue) {
+        const keyValueArray = [
+            ["--font-size-16", (16 + diffValue).toString() + "px"],
+            ["--font-size-18", (18 + diffValue).toString() + "px"],
+            ["--font-size-20", (20 + diffValue).toString() + "px"],
+            ["--font-size-25", (25 + diffValue).toString() + "px"]
+          ];
+          this.setPropertiesFromArray(keyValueArray);
+    },
+
+    updatePageTheme(isEnabled) {
+        const keyValueArray = [
+          //                                                 Dark theme   Light theme
+          ["--dark-100",            ((isEnabled) ?            "#161616" : "#E9E9E9")],
+          ["--dark-150",            ((isEnabled) ?            "#202020" : "#DFDFDF")],
+          ["--dark-200",            ((isEnabled) ?            "#282828" : "#D7D7D7")],
+          ["--dark-250",            ((isEnabled) ?            "#333333" : "#CCCCCC")],
+          ["--dark-300",            ((isEnabled) ?            "#3D3D3D" : "#C2C2C2")],
+          ["--dark-400",            ((isEnabled) ?            "#575757" : "#C8C8C8")],
+          ["--dark-500",            ((isEnabled) ?            "#717171" : "#8E8E8E")],
+          ["--dark-600",            ((isEnabled) ?            "#8B8B8B" : "#747474")],
+
+          ["--slider-grey",         ((isEnabled) ?            "#888888" : "#00CFCF")],
+          ["--slider-track",        ((isEnabled) ?            "#AAAAAA" : "#D7D7D7")],
+          ["--slider-track-active", ((isEnabled) ?            "#FF5050" : "#00CFCF")],
+
+          ["--hover-colour",        ((isEnabled) ?            "#FFFFFF" : "#000000")],
+
+          ["--fg",                  ((isEnabled) ?            "#EEEEEE" : "#111111")],
+          ["--fg-light",            ((isEnabled) ?            "#FFFFFF" : "#000000")],
+          ["--fg-dark",             ((isEnabled) ?            "#CCCCCC" : "#333333")],
+          ["--fg-darker",           ((isEnabled) ?            "#AAAAAA" : "#555555")]
+        ];
+        this.setPropertiesFromArray(keyValueArray);
+    },
+
+    setPropertiesFromArray(keyValueArray) {
+        for (const [key, value] of keyValueArray) {
+            // Call the setProperty export function for each key-value pair
+            document.documentElement.style.setProperty(key, value);
+        }
+    },
+
+    // Returns value stored in Cookie for given key, otherwise returns default value
+    getCookieValue(cookieName, defaultValue) {
+        const cookieValue = Cookies.get(cookieName);
+        return ((cookieValue !== undefined && cookieValue !== NaN) ? cookieValue : defaultValue);
+    },
+
+    convertSliderValueToDiff(sliderValue) {
+        const keyValueArray = {
+            1: -6,
+            2: -3,
+            3:  0,
+            4:  3,
+            5:  6
+        };
+        const newDiffValue = keyValueArray[sliderValue];
+        return ((newDiffValue !== undefined) ? newDiffValue : 0);
+    },
+
+    convertThemeToBool(themeValue) {
+        return ((themeValue === "Light") ? false : true);
+    }
+}
--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,14 +1,109 @@
 import React from "react";
-import Layout from "../Layout";
+import { useState, useEffect, useRef } from "react";
 
-// TODO: Settings page
+import Switch from "react-switch";
+import ReactSlider from "react-slider";
+import Cookies from "js-cookie";
+
+import Layout from "../Layout";
+import '../css/Settings.css';
+
+import { Helper } from '../PageStyleHelper.js';
 
 function Settings(props) {
-    return (
-        <Layout search={props.search}>
-            <h1>Not implemented yet</h1>
-        </Layout>
-    );
+  // Initialize states for theme, slider and slider's label
+  const [isEnabled  , setIsEnabled  ] = useState(false);
+  const [sliderValue, setSliderValue] = useState(3);
+  const [displayText, setDisplayText] = useState("Normal");
+  const [diffValue  , setDiffValue  ] = useState(0);
+
+  const initializedRef = useRef(false);
+
+  useEffect(() => {
+    // Ensure execution only once on page load
+    if (!initializedRef.current) {
+      // Set initial values stored in Cookies
+      setIsEnabled(Helper.convertThemeToBool(Helper.getCookieValue('selectedTheme', "Dark")));
+      setSliderValue(parseInt(Helper.getCookieValue('sliderValue', 3)));
+      initializedRef.current = true;
+    }
+  }, []);
+
+  useEffect(() => {
+    // Store slider value to Cookie
+    Cookies.set('sliderValue', sliderValue);
+
+    const keyValueArray = {
+      1: "Very small",
+      2: "Small",
+      3: "Normal",
+      4: "Larger",
+      5: "Large"
+    };
+    // Set new values for state variables from above array
+    setDiffValue(Helper.convertSliderValueToDiff(sliderValue));
+
+    const newSliderText = keyValueArray[sliderValue];
+    setDisplayText((newSliderText !== undefined) ? newSliderText : "Normal");
+  }, [sliderValue]);
+
+  useEffect(() => {
+    // On diffValue change, recalculate font sizes
+    Helper.updateFontSize(diffValue);
+  }, [diffValue]);
+
+  useEffect(() => {
+    // Store the theme value to Cookie
+    Cookies.set('selectedTheme', ((isEnabled) ? "Dark" : "Light"));
+
+    Helper.updatePageTheme(isEnabled);
+  }, [isEnabled]);
+
+  return (
+    <Layout search={props.search}>
+      <h1>Settings</h1>
+
+      <div className='settings'>
+          <div className='settings-button'>
+            <label style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px' }}>Select colour theme</span>
+              <Switch
+                onChange=       {setIsEnabled}
+                checked=        {isEnabled}
+                uncheckedIcon=  {<span role="img" aria-label="dark-theme">🌙</span>}
+                checkedIcon=    {<span role="img" aria-label="light-theme">⛅</span>}
+
+                onHandleColor=  {((!isEnabled) ? "#00CFCF" : "#ff5050")}
+                offHandleColor= {((!isEnabled) ? "#00CFCF" : "#ff5050")}
+                offColor=       {((!isEnabled) ? "#D7D7D7" : "#888888")}
+                onColor=        {((!isEnabled) ? "#D7D7D7" : "#888888")}
+
+                width=          {62}
+              />
+            </label>
+          </div>
+
+          <div className='settings-button' style={{ marginTop: '20px', display: 'flex'}}>
+              <span style={{ marginRight: '10px' }}>Select text size</span>
+              <div style={{display: 'flex', width: '300px', flexDirection: 'column', alignItems: 'center', textAlign: 'center'}}>
+                <ReactSlider
+                  className=      "horizontal-slider"
+                  thumbClassName= "slider-thumb"
+                  trackClassName= "slider-track"
+
+                  min=            {1}
+                  max=            {5}
+                  step=           {1}
+
+                  onChange=       {setSliderValue}
+                  value=          {sliderValue}
+                />
+                <p>{displayText}</p>
+              </div>
+          </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default Settings;
