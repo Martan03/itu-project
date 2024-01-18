23aeca6f46f542873e60aa8fc6ecbb5ff8a51bef
Tomas Daniel <52777834+Dandys3900@users.noreply.github.com>
2024-01-18 18:20:19 +0100

    CRUD via Calendar (#17)
    * Add option to create and edit vacations directly from calendar - TO BE TESTED!
    
    * Update
    
    * Save vacations
    
    * Add delete option.
    
    * Date changes
    
    * Remove debug logs.
    
    ---------
    
    Co-authored-by: Dandys <tomasdaniel7800@gmail.com>
    Co-authored-by: Martan03 <martinslezak03@gmail.com>

---
+++ frontend/src/components/BottomModal.js
@@ -0,0 +1,110 @@
+/**
+ * ITU project
+ *
+ * Tomáš Daniel <xdanie14>
+ */
+
+import React, { useState } from 'react';
+import { ReactComponent as TrashIcon } from '../icons/trash.svg';
+import { TitleInput, DescInput } from "../components/Input";
+import { DateRange } from "../components/DateRange";
+import '../css/BottomModal.css';
+import { getVacation, saveVacation, deleteVacation } from '../Db';
+
+const BottomModal = ({id, setId, setEvents}) => {
+  const [data, setData] = useState(null);
+
+  useState(() => {
+    // TODO: handle errors and loading
+    getVacation(id, setData, () => {}, () => {});
+  }, [id])
+
+  const closeModal = () => {
+    setId(null);
+  };
+
+  const closeOnOutsideClick = (event) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  };
+
+  const inputChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const saveData = () => {
+    setEvents(prev => {
+      return prev.map(item => {
+        if (item.id === id) {
+          const start = new Date(data.start_date);
+          start.setHours(0, 0, 0, 0);
+
+          const end = new Date(data.end_date);
+          end.setHours(23, 59, 59);
+
+          return {
+            id: data.id,
+            title: data.title ?? 'No title',
+            start,
+            end,
+          }
+        }
+        return item;
+      });
+    })
+    saveVacation(data);
+  };
+
+  const removeTrip = () => {
+    setEvents((prev) => {
+      return prev.filter((item) => item.id !== id);
+    });
+    deleteVacation(id);
+    closeModal();
+  };
+
+  return (
+    <div className='modal'>
+      {data ? (
+        <div onClick={closeOnOutsideClick} style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
+        {/* Modal content */}
+            <div className="modal-content">
+              <div className="modal-header">
+                <span className="close" onClick={closeModal}>✔</span>
+                <DateRange input
+                  values={[data.start_date, data.end_date]}
+                  onChange={inputChange}
+                  onBlur={saveData}
+                />
+                <TitleInput
+                  onChange={inputChange}
+                  onBlur={saveData}
+                  value={data.title}
+                />
+              </div>
+              <div className="modal-body">
+                <div className='vacation-header'>
+                <DescInput
+                  onChange={inputChange}
+                  onBlur={saveData}
+                  value={data.description}
+                />
+                </div>
+              </div>
+              <div className="modal-footer">
+                  <button
+                      onClick={removeTrip}
+                      className="remove-trip-button">
+                      <TrashIcon/>
+                  </button>
+              </div>
+            </div>
+          </div>
+      ) : ''}
+    </div>
+  );
+};
+
+export default BottomModal;
--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -4,7 +4,7 @@
  * Tomáš Daniel <xdanie14>
  */
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment, useCallback } from "react";
 import Layout from "../Layout";
 import { useNavigate } from "react-router-dom";
 import {
@@ -16,8 +16,15 @@ import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../css/Calendar.css';
 
+//import CalendarEdit from "../components/CalendarEdit.js";
+import BottomModal from "../components/BottomModal.js";
+
+import { saveVacation } from "../Db";
+
 function Calendar(props) {
     const [events, setEvents] = useState(null);
+    const [modalId, setModalId] = useState(null);
+
     const [loading, setLoading] = useState(true);
 
     const nav = useNavigate();
@@ -32,11 +39,17 @@ function Calendar(props) {
             .then((data) => {
                 var events = [];
                 for (let vacation of data) {
+                    const start = new Date(vacation.start_date);
+                    start.setHours(0, 0, 0, 0);
+
+                    const end = new Date(vacation.end_date);
+                    end.setHours(23, 59, 59, 0);
+
                     events.push({
                         id: vacation.id,
-                        start: new Date(vacation.start_date),
-                        end: new Date(vacation.end_date),
-                        title: vacation.title,
+                        start,
+                        end,
+                        title: vacation.title ?? "No title",
                     });
                 }
                 setEvents(events);
@@ -54,22 +67,71 @@ function Calendar(props) {
     })
     const localizer = momentLocalizer(moment);
 
-    const selectEvent = (e) => {
-       nav(`/vacation?id=${e.id}`);
-    }
+    const handleSelectSlot = useCallback(
+        ({ start, end }) => {
+          const start_date = new Date(start);
+          start_date.setHours(0, 0, 0, 0);
+
+          const end_date = new Date(end);
+          end_date.setSeconds(end_date.getSeconds() - 1);
+          end_date.setHours(23, 59, 59);
+
+          saveVacation({start_date: start_date, end_date: end_date}).then(id => {
+            setModalId(id);
+            setEvents(prev => {
+                return [
+                    ...prev,
+                    {
+                        id: id,
+                        start: start_date,
+                        end: end_date,
+                        title: 'No title'
+                    },
+                ]
+            });
+          });
+        },
+        []
+      )
+
+      const handleSelectEvent = useCallback(
+        ({id}) => {
+            setModalId(id);
+        },
+        []
+      )
+
+    const doubleClickEvent = (e) => {
+        nav(`/vacation?id=${e.id}`);
+      };
 
     return (
-        <Layout search={props.search} menu={props.menu}>
-            { loading && <h2>Loading...</h2> }
-            { events &&
-                <CalendarComponent
-                    className="calendar"
-                    localizer={localizer}
-                    events={events}
-                    onSelectEvent={selectEvent}
-                />
-            }
-        </Layout>
+        <Fragment>
+            <Layout search={props.search} menu={props.menu}>
+                { loading && <h2>Loading...</h2> }
+                { events &&
+                    <div className="calendar-wrap">
+                        <CalendarComponent
+                            dayLayoutAlgorithm={'no-overlap'}
+                            className="calendar"
+                            localizer={localizer}
+                            events={events}
+                            onSelectEvent={handleSelectEvent}
+                            onSelectSlot={handleSelectSlot}
+                            onDoubleClickEvent={doubleClickEvent}
+                            selectable
+                        />
+                        {modalId && (
+                            <BottomModal
+                                id={modalId}
+                                setId={setModalId}
+                                setEvents={setEvents}
+                            />
+                        )}
+                </div>
+                }
+            </Layout>
+        </Fragment>
     );
 }
 
